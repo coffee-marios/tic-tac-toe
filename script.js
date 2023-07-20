de088c9c888e9bb8272022e7be621961d7ed5394
coffee-marios <mark_theo@yahoo.com>
2023-07-20 11:38:00 +0300

    Add data attributes to the board

--- script.js
+++ script.js
@@ -1,16 +1,37 @@
-// Create a board
 (function () {
-  var board = document.getElementById("gameboard");
+  var gaming = {
+    Gameboard: [2, 2],
+    playerA: [],
+    playerB: [],
 
-  for (let i = 0; i < 3; i++) {
-    const blockBoard = document.createElement("div");
-    blockBoard.classList.add("block-board");
-    board.appendChild(blockBoard);
+    init: function () {
+      this.cacheDom();
+      this.drawBoard();
+    },
+    cacheDom: function () {
+      this.board = document.getElementById("gameboard");
+    },
 
-    for (let j = 0; j < 3; j++) {
-      const blockBoard = document.createElement("div");
-      blockBoard.classList.add("block-board");
-      board.appendChild(blockBoard);
-    }
-  }
+    drawBoard: function () {
+      console.log(this.board, 3);
+
+      for (let i = 0; i < 9; i++) {
+        this.blockBoard = document.createElement("div");
+        this.blockBoard.classList.add("block-board");
+        this.blockBoard.dataset.idOfBlock = i;
+
+        this.board.appendChild(this.blockBoard);
+        this.blockBoard.onclick = this.drawBlock.bind(
+          this,
+          this.blockBoard.dataset.idOfBlock
+        );
+      }
+    },
+    drawBlock: function (i) {
+      var getBlock = this.board.querySelector(`div[data-id-Of-Block='${i}']`);
+      getBlock.innerText = "X";
+      console.log(getBlock);
+    },
+  };
+  gaming.init();
 })();
