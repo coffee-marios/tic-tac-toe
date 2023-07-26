c3a346260bf5f7f37567ae40426d39c2a887d521
coffee-marios <mark_theo@yahoo.com>
2023-07-26 09:44:40 +0300

    Allow the event handler to toggle the messages we get when we click on the board

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
 (function () {
   console.log("OK");
+
   var gaming = {
     Gameboard: [2, 2],
-    playerA: [],
-    playerB: [],
+    players: function (name, turn = true, score = 3) {
+      this.turn = turn;
+      this.score = score;
+      this.name = name;
+      function addScore() {
+        this.score += 3;
+        return this.score;
+      }
+      function giveBack() {
+        this.turn = !this.turn;
+        return () => this.turn;
+      }
+      return { turn, score, name, giveBack, addScore };
+    },
+
+    statistics: function () {
+      this.Marios = this.players("Marios");
+      this.Enemy = this.players("Enemy");
+    },
 
     init: function () {
       this.cacheDom();
       this.drawBoard();
+      this.statistics();
     },
     cacheDom: function () {
       this.board = document.getElementById("gameboard");
     },
     bindEvents: function (i) {
-      this.blockBoard.addEventListener("click", () => {
+      const once = {
+        once: true,
+      };
+      const clickBlock = () => {
         this.drawBlock(i);
-      });
+      };
+      this.blockBoard.addEventListener("click", clickBlock, once);
+      // this.blockBoard.removeEventListener("click", clickBlock);
     },
 
     drawBoard: function () {
@@ -25,17 +49,19 @@
         this.blockBoard.dataset.idOfBlock = i;
         this.board.appendChild(this.blockBoard);
         this.bindEvents(this.blockBoard.dataset.idOfBlock);
-
-        // Also works, without bindEvents
-        // this.blockBoard.onclick = this.drawBlock.bind(
-        //   this,
-        //   this.blockBoard.dataset.idOfBlock
-        // );
       }
     },
     drawBlock: function (i) {
       var getBlock = this.board.querySelector(`div[data-id-Of-Block='${i}']`);
-      getBlock.innerText = "X";
+
+      console.log(`drawBlock: ${this.Marios.giveBack}`);
+
+      this.Marios.giveBack()()
+        ? (getBlock.innerText = "X")
+        : (getBlock.innerText = "O");
+
+      console.log(this.Marios.addScore());
+
       console.log(getBlock);
     },
   };
